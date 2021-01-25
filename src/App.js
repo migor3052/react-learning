79826af7826c7effa86c7cfe0e6b6de364fa2e5b
Michael Gordon <michael.gordon.1993@gmail.com>
2021-01-25 14:00:25 -0500

    Finished Chapter 4

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
 import './App.css';
-import react from "./react.png";
-function Header(props) {
-  return(
-    <header>
-      <h1>{props.name}'s Place</h1>
-    </header>
-  )
-}
-
-function Main(props){
-  return(
-    <section>
-      <p>The best place there is. It's {props.adjective}</p>
-      <img src={react} height={150} alt="react logo"/>
-      <ul style={{textAlign:"left"}}>
-        {props.tools.map(tool => (
-        <li key={tool.id}>{tool.title}</li>
-        ))}
-      </ul>
-    </section>
-  )
-}
-
-function Footer(props){
-  return(
-    <p>Copyright {props.year}</p>
-  )
-}
-const tools = [
-  "Hammer", 
-  "Sickle",
-  "Saw"
-]
-
-const toolsObjects = tools.map((tool, i) => ({ id: i, title: tool }));
-console.log(toolsObjects);
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+  console.log(emotion);
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`)
+  }, [emotion]);
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`)
+  }, [secondary]);
+
   return (
-    <div className="App">
-      <Header name="Michael"/>
-      <Main adjective="Amazing" tools={toolsObjects}/>
-      <Footer year={new Date().getFullYear()}/>
-    </div>
+    <>
+      <h1>Hello, emotion is {emotion} and {secondary}</h1>
+      <button onClick={() => setEmotion("Frustrated")}>
+        Frustrate
+      </button>
+      <button onClick={()=> setEmotion("Enthusiastic")}>Enthuse</button>
+      <button onClick={() => setSecondary("Crabby")}>Make crabby</button>
+    </>
   );
 }
 
