4761d25e51af446de34d37915d3b2b11e3c6ba95
Michael Gordon <michael.gordon.1993@gmail.com>
2021-01-25 10:14:48 -0500

    Finished Chapter 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import react from "./react.png";
+function Header(props) {
+  return(
+    <header>
+      <h1>{props.name}'s Place</h1>
+    </header>
+  )
+}
+
+function Main(props){
+  return(
+    <section>
+      <p>The best place there is. It's {props.adjective}</p>
+      <img src={react} height={150} alt="react logo"/>
+      <ul style={{textAlign:"left"}}>
+        {props.tools.map(tool => (
+        <li key={tool.id}>{tool.title}</li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+function Footer(props){
+  return(
+    <p>Copyright {props.year}</p>
+  )
+}
+const tools = [
+  "Hammer", 
+  "Sickle",
+  "Saw"
+]
+
+const toolsObjects = tools.map((tool, i) => ({ id: i, title: tool }));
+console.log(toolsObjects);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Michael"/>
+      <Main adjective="Amazing" tools={toolsObjects}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
